ddc0ee74bf769fbe66bd692878924308ee17f835
ricardoyepes7 <ricardoyepes7@hotmail.com>
2023-03-16 15:50:56 -0500

    Add: Eureka-Config

--- car-microservice/src/main/java/com/tutorial/carmicroservice/CarMicroserviceApplication.java
+++ car-microservice/src/main/java/com/tutorial/carmicroservice/CarMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.tutorial.carmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CarMicroserviceApplication {
 
 	public static void main(String[] args) {
