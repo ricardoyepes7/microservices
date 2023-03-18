4a1305b4999792b93324124f4a8c548c1858ad1c
ricardoyepes7 <ricardoyepes7@hotmail.com>
2023-03-17 19:07:07 -0500

    Multi-Instancies

--- user-microservice/src/main/java/com/tutorial/usermicroservice/feingclients/BikeFeingClient.java
+++ user-microservice/src/main/java/com/tutorial/usermicroservice/feingclients/BikeFeingClient.java
@@ -1,17 +1,12 @@
 package com.tutorial.usermicroservice.feingclients;
 
 import com.tutorial.usermicroservice.dto.BikeDto;
-import com.tutorial.usermicroservice.dto.CarDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "bike-service",url = "http://localhost:8003/bike-service/bikes")
+@FeignClient(name = "bike-service",path = "/bikes")
 public interface BikeFeingClient {
     @GetMapping("/user/{userId}")
     List<BikeDto> getBikesByUserId(@PathVariable("userId") long userId);
--- user-microservice/src/main/java/com/tutorial/usermicroservice/feingclients/CarFeingClient.java
+++ user-microservice/src/main/java/com/tutorial/usermicroservice/feingclients/CarFeingClient.java
@@ -1,16 +1,12 @@
 package com.tutorial.usermicroservice.feingclients;
 
-import com.tutorial.usermicroservice.dto.BikeDto;
 import com.tutorial.usermicroservice.dto.CarDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "car-service",url = "http://localhost:8002/car-service/cars")
+@FeignClient(name = "car-service", path = "/cars")
 public interface CarFeingClient {
     @GetMapping("/user/{userId}")
     List<CarDto> getCarsByUserId(@PathVariable("userId") long userId);
